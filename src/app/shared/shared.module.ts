fc297dd0f1869475801794419e5b25f5af0d82ef
tomerickson <tomerickson42@gmail.com>
2022-10-11 13:58:42 -0400

    updates

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,33 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card'
-import { MatCommonModule } from '@angular/material/core';
-import { MatListModule } from '@angular/material/list';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MaterialModule } from './material.module';
 import { HeaderComponent } from './header/header.component';
-const materialModules = [
-  MatCardModule,
-  MatCommonModule,
-  MatListModule,
-  MatRadioModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatToolbarModule
-];
-
-const ngrxModules: [] = [];
-
-const allModules = materialModules.concat(CommonModule).concat(ngrxModules)
 
 @NgModule({
-
-  imports: [allModules],
-  exports: [allModules, HeaderComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [CommonModule, MaterialModule, HeaderComponent],
   declarations: [HeaderComponent]
-
 })
-export class SharedModule {
- }
+export class SharedModule {}
