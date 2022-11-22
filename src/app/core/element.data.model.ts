d6344061e1ca3efe8d91f29438291ff6049f120e
tomerickson <tomerickson42@gmail.com>
2022-11-22 15:41:40 -0500

    update dev dependencies, add elementdata model

---
+++ src/app/core/element.data.model.ts
@@ -0,0 +1,21 @@
+export interface ElementDataModel {
+  Z: number;
+  E: string;
+  EName: string;
+  P: number;
+  G: number;
+  AWeight: number;
+  ARadius: number;
+  MolarVolume: number;
+  Melting: number;
+  Boiling: number;
+  Negativity: number;
+  Affinity: number;
+  Valence: number;
+  MaxIonNum: number;
+  MaxIonization: number;
+  STPDensity: number;
+  ElectConduct: number;
+  ThermConduct: number;
+  SpecHeat: number;
+}
