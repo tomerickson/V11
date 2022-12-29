839f716de00ebd82a57ce35c7e1dd3bb8dd1ee4c
tomerickson <tomerickson42@gmail.com>
2022-12-29 17:27:11 -0500

    latest ngrx

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,8 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
+import { TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import 'zone.js/testing';
+import 'zone.js/dist/async-test.js'
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
