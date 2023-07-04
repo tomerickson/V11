e95e242ae3f6e24a912a964a600e919a1a94d960
tomerickson <tomerickson42@gmail.com>
2023-07-04 17:33:50 -0400

    adding lenr-events feature

--- src/app/core/models/downloadable.model.ts
+++ src/app/core/models/downloadable.model.ts
@@ -13,7 +13,13 @@ export interface Downloadable {
 }
 
 export const downloadOptions: Downloadable[] = [
-  { id: 0, key: 'csv', name: 'Comma-delimited', extension: '.csv', type: 'text/csv' },
+  {
+    id: 0,
+    key: 'csv',
+    name: 'Comma-delimited',
+    extension: '.csv',
+    type: 'text/csv'
+  },
   {
     id: 1,
     key: 'tab',
@@ -22,13 +28,18 @@ export const downloadOptions: Downloadable[] = [
     type: 'text/tab-separated-values'
   },
   { id: 2, key: 'txt', name: 'Text', extension: '.txt', type: 'text/html' },
-  { id: 3, key: 'prettyjson', name: 'Pretty JSON', extension: '.json', type: 'application/json' },
+  {
+    id: 3,
+    key: 'prettyjson',
+    name: 'Pretty JSON',
+    extension: '.json',
+    type: 'application/json'
+  },
   {
     id: 4,
     key: 'json',
     name: 'Compressed JSON',
     extension: '.json',
     type: 'application/json'
-  },
-  { id: 5, key: 'txt', name: 'any', extension: '.?', type: 'text/html' }
+  }
 ];
