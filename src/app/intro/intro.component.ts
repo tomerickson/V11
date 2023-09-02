e2bba6420b4f7a555ca7130acf20ca7ca56f3db7
Tom Erickson <tomerickson42@gmail.com>
2023-09-01 21:32:39 -0400

    add staging configuration

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { Store } from '@ngrx/store';
 import { IPageHeader } from '../core/ipage-header';
 import { HeaderProviderService } from '../shared/header/header.provider.service';
+import { AppConfigService } from '../core/config/app-config.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatExpansionModule],
+  imports: [CommonModule, MatCardModule, MatExpansionModule, RouterModule],
   selector: 'mfmp-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss'],
@@ -17,10 +19,15 @@ import { HeaderProviderService } from '../shared/header/header.provider.service'
   ]
 })
 export class IntroComponent implements OnInit {
-  store: Store = Inject(Store);
+
+  readonly apiUrl!: string;
+  store = inject(Store);
+  config = inject(AppConfigService);
   headerInfo: IPageHeader | undefined;
 
-  constructor( private headerService: HeaderProviderService) {}
+  constructor( private headerService: HeaderProviderService) {
+    this.apiUrl = this.config.apiUrl;
+  }
 
   ngOnInit(): void {
     this.headerService.buildPageHeader('intro');
