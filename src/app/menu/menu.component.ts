bfcb68ac753889ad12d6fcb15832c0a08a530462
tomerickson <tomerickson42@gmail.com>
2023-12-31 20:07:13 -0500

    add cascades submenu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,14 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild, inject } from '@angular/core';
-import { MatListModule } from '@angular/material/list';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  ViewChild,
+  inject,
+  signal
+} from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule, MatNavList } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
-import { MenuItemComponent } from './menu-item.component';
-import { MatIconModule } from '@angular/material/icon';
-import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { AppConfigService } from '../../app/core/config/app-config.service';
-import { unescapeLeadingUnderscores } from 'typescript';
 import { IAppConfig } from '../core/config/iapp-config.model';
+import { MenuItemComponent } from './menu-item.component';
 @Component({
   standalone: true,
   selector: 'mfmp-menu',
@@ -20,23 +25,49 @@ import { IAppConfig } from '../core/config/iapp-config.model';
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    MenuItemComponent,
-  ],
+    MenuItemComponent
+  ]
 })
-export class MenuComponent {
-
+export class MenuComponent implements AfterViewInit {
   readonly appService = inject(AppConfigService);
   config: IAppConfig;
-  showCascades = false;
+  showCascades = signal(false);
   isShowing = true;
   isExpanded = false;
-  @ViewChild('cascades') cascades!: ElementRef
+  @ViewChild('cascades') cascades!: ElementRef<HTMLDivElement>;
 
   constructor() {
     this.config = this.appService.config;
   }
-  toggleMenu() {
-    this.showCascades = !this.showCascades;
-    console.log('togglemenu');
+
+  ngAfterViewInit(): void {
+    console.log(this.cascades);
+  }
+
+  toggleMenu(event: Event | false) {
+    let cascadesList = this.cascades.nativeElement;
+    let show = this.showCascades();
+    let visible = !cascadesList.classList.contains('collapse');
+
+    if (event instanceof Event) {
+      // Toggle cascades visibility
+      this.showCascades.set(!this.showCascades());
+      event.stopPropagation();
+    } else {
+      // Hide cascades
+      this.showCascades.set(false);
+    }
+
+    if (this.showCascades() != show )
+      if (this.showCascades()) {
+        cascadesList.classList.remove('collapse');
+      } else {
+        cascadesList.classList.add('collapse');
+      }
+    }
+
+  restoreMenu() {
+    console.log(this.cascades);
+    this.showCascades.set(false);
   }
 }
