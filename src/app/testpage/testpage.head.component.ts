f3fd44cb46bb096136c25441cf415c63d2134e5e
tomerickson <tomerickson42@gmail.com>
2025-04-20 10:03:39 -0400

    framework uplift

--- src/app/testpage/testpage.head.component.ts
+++ src/app/testpage/testpage.head.component.ts
@@ -1,18 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MyTelInput } from '../shared/custom/form-field-custom-control-example';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 @Component({
-    selector: 'mfmp-testpage',
-    imports: [CommonModule, ReactiveFormsModule, MyTelInput],
-    template: `<example-tel-input placeholder=""></example-tel-input>
+  selector: 'mfmp-testpage',
+  imports: [CommonModule, ReactiveFormsModule, MatButtonModule, MatCardModule],
+  template: `
+    <mat-card class="fullheight">
+      <mat-card-content>Content</mat-card-content>
+      <mat-card-actions align="end">
+        <div>
+          <button mat-raised-button>Action</button>
+        </div>
+      </mat-card-actions>
+    </mat-card>
   `,
-    styleUrls: []
+  styles: `
+    .fullheight {
+      bottom: 0;
+      right: 0;
+    }
+    .bottom-dock {
+      bottom: 5;
+      right: 5;
+    }
+  `
 })
 export class TestpageHeadComponent {
   formGroup = this.fb.group({
     counter: 0,
-    options: ['Core',[Validators.required]]
+    options: ['Core', [Validators.required]]
   });
 
   get options(): FormControl {
